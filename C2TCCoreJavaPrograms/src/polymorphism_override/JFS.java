3339a3511249daa698f9237debacba30909e2cb9
lenov <lenov@Idepad_Gaming_3>
2025-07-28 16:30:40 +0530

    Polymorphism_override

---
+++ C2TCCoreJavaPrograms/src/polymorphism_override/JFS.java
@@ -0,0 +1,8 @@
+package polymorphism_override;
+
+public class JFS extends PLTraining
+{
+	public void session(String language) {
+		System.out.println("The session going from"+language+"in");
+	}
+}
