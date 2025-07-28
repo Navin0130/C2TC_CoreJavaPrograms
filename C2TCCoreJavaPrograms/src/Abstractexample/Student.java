7953d0fcf3c6e834354687d24154be37db09a0f8
lenov <lenov@Idepad_Gaming_3>
2025-07-28 16:34:46 +0530

    Abstract Example

---
+++ C2TCCoreJavaPrograms/src/Abstractexample/Student.java
@@ -0,0 +1,9 @@
+package Abstractexample;
+
+public class Student extends Coder {
+	public void rules() {
+		System.out.println("Morning wake up at"+wakeup);
+		System.out.println("upload the file");
+	}
+
+}
